62a2b52265c176d6c20d502c831c5ae021953050
Frank Chlebda <frankchlebda@gmail.com>
2025-05-04 16:14:54 +0200

    update

---
+++ projekt2025/JS/script.js
@@ -0,0 +1,89 @@
+const godOfWar = {
+    price: 90,
+    releaseDate: 2018,
+    rating: "9/10"
+
+};
+const Witcher3 = {
+    price: 110,
+    releaseDate: 2015,
+    rating: "9,2/10"
+
+};
+const MetalGearSolid3 =
+{
+    price: 70,
+    releaseDate: 2004,
+    rating: "9,2/10"
+};
+const Skyrim =
+{
+    price: 69,
+    releaseDate: 2011,
+    rating: "9,1/10"
+};
+const nfsMostWanted =
+{
+    price: 43,
+    releaseDate: 2005,
+    rating: "8,4/10"
+
+};
+const oblivionRemastered =
+{
+    price: 200,
+    releaseDate: 2025,
+    rating: "6,1/10"
+};
+const Cs2 =
+{
+    price: 0,
+    releaseDate: 2023,
+    rating: "8,6/10"
+
+};
+const jediSurvivor =
+{
+    price: 85,
+    releaseDate: 2023,
+    rating: "7,5/10"
+};
+const etGame =
+{
+    price: 40,
+    releaseDate: 1982,
+    rating: "1/10"
+
+};
+const redDeadRedemption =
+{
+    price: 120,
+    releaseDate: 2010,
+    rating: "9/10"
+
+};
+const euroTruckSim2 =
+{
+    price: 70,
+    releaseDate: 2012,
+    rating: "9,8/10"
+
+};
+const undertale =
+{
+    price: 47,
+    releaseDate: 2015,
+    rating: "10/10"
+
+
+};
+const games = [
+    oblivionRemastered,
+    Cs2,
+    jediSurvivor,
+    etGame,
+    redDeadRedemption,
+    euroTruckSim2,
+    undertale
+];
+document.getElementById("GameTabel").innerHTML = Witcher3;
