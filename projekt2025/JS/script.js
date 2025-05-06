7eb84ceef5d90b8a36c6e4786148876cc5e68b92
Frank Chlebda <frankchlebda@gmail.com>
2025-05-06 12:18:05 +0200

    update

--- projekt2025/JS/script.js
+++ projekt2025/JS/script.js
@@ -1,89 +1,102 @@
-const godOfWar = {
-    price: 90,
-    releaseDate: 2018,
-    rating: "9/10"
 
-};
-const Witcher3 = {
-    price: 110,
-    releaseDate: 2015,
-    rating: "9,2/10"
+const storeGames = [
+    {
+        id: 101,
+        name: "God of war",
+        price: 90,
+        releaseDate: "2018",
+        rating: "9/10",
 
-};
-const MetalGearSolid3 =
-{
-    price: 70,
-    releaseDate: 2004,
-    rating: "9,2/10"
-};
-const Skyrim =
-{
-    price: 69,
-    releaseDate: 2011,
-    rating: "9,1/10"
-};
-const nfsMostWanted =
-{
-    price: 43,
-    releaseDate: 2005,
-    rating: "8,4/10"
+    },
+    {
+        id: 102,
+        name: "Witcher 3",
+        price: 110,
+        releaseDate: "2015",
+        rating: "9,2/10"
 
-};
-const oblivionRemastered =
-{
-    price: 200,
-    releaseDate: 2025,
-    rating: "6,1/10"
-};
-const Cs2 =
-{
-    price: 0,
-    releaseDate: 2023,
-    rating: "8,6/10"
+    },
+    {
+        id: 103,
+        name: "Metal Gear Solid 3",
+        price: 70,
+        releaseDate: "2004",
+        rating: "9,2/10"
 
-};
-const jediSurvivor =
-{
-    price: 85,
-    releaseDate: 2023,
-    rating: "7,5/10"
-};
-const etGame =
-{
-    price: 40,
-    releaseDate: 1982,
-    rating: "1/10"
+    },
+    {
+        id: 104,
+        name: "Skyrim",
+        price: 69,
+        releaseDate: "2011",
+        rating: "9,1/10"
 
-};
-const redDeadRedemption =
-{
-    price: 120,
-    releaseDate: 2010,
-    rating: "9/10"
+    },
+    {
+        id: 105,
+        name: "NFS Most Wanted",
+        price: 43,
+        releaseDate: "2005",
+        rating: "8,4/10"
 
-};
-const euroTruckSim2 =
-{
-    price: 70,
-    releaseDate: 2012,
-    rating: "9,8/10"
+    },
+    {
+        id: 106,
+        name: "Oblivion Remastered",
+        price: 200,
+        releaseDate: "2025",
+        rating: "6,1/10"
+    },
+    {
+        id: 107,
+        name: "Cs2",
+        price: 0,
+        releaseDate: "2023",
+        rating: "8,6/10"
 
-};
-const undertale =
-{
-    price: 47,
-    releaseDate: 2015,
-    rating: "10/10"
+    },
+    {
+        id: 108,
+        name: "Jedi Survivior",
+        price: 85,
+        releaseDate: "2023",
+        rating: "7,5/10"
+
+    },
+    {
+        id: 109,
+        name: "ET Game",
+        price: 40,
+        releaseDate: "1982",
+        rating: "1/10"
+
+    },
+    {
+        id: 110,
+        name: "Red Dead Redemption ",
+        price: 120,
+        releaseDate: "2010",
+        rating: "9/10"
+
+    },
+    {
+        id: 110,
+        name: "Euro Truck Sim.2",
+        price: 70,
+        releaseDate: "2012",
+        rating: "9,8/10"
+
+    },
+    {
+        id: 110,
+        name: "Undertale",
+        price: 47,
+        releaseDate: "2015",
+        rating: "10/10"
+
+    }
 
 
-};
-const games = [
-    oblivionRemastered,
-    Cs2,
-    jediSurvivor,
-    etGame,
-    redDeadRedemption,
-    euroTruckSim2,
-    undertale
+
 ];
-document.getElementById("GameTabel").innerHTML = Witcher3;
+document.getElementById("GameTabel").innerHTML = storeGames;
